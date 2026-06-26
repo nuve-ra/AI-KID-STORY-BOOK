5de1f4eee1c7432ebc72a16a41f099fed3baff4c
Nupoor Verma <nuve19ec@cmrit.ac.in>
2026-06-26 13:47:17 +0530

    fixed api problem of gemini api

--- config/Geminiai.tsx
+++ config/Geminiai.tsx
@@ -1,7 +1,10 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const apiKey =
-  process.env.GEMINI_API_KEY || '';
+const apiKey = process.env.GEMINI_API_KEY;
+
+if (!apiKey) {
+  throw new Error("GEMINI_API_KEY is missing");
+}
 
 const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
