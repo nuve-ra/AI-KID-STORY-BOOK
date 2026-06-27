c88d9fd54768c34055c7a2f605f99ef4f9345fde
Nupoor Verma <nuve19ec@cmrit.ac.in>
2026-06-27 15:37:57 +0530

    fixed the gemini api bug

--- config/Geminiai.tsx
+++ config/Geminiai.tsx
@@ -1,15 +1,4 @@
-// import { GoogleGenerativeAI } from '@google/generative-ai';
 
-// const apiKey = process.env.GEMINI_API_KEY;
-// console.log("API Key exists:", !!process.env.GEMINI_API_KEY);
-// if (!apiKey) {
-//   throw new Error("GEMINI_API_KEY is missing");
-// }
-
-// const genAI = new GoogleGenerativeAI(apiKey);
-// const model = genAI.getGenerativeModel({
-//   model: "gemini-2.5-flash",
-// });
 import { GoogleGenAI } from "@google/genai";
 
 const apiKey = process.env.GEMINI_API_KEY;
@@ -21,11 +10,5 @@ if (!apiKey) {
 export const ai = new GoogleGenAI({
   apiKey,
   });
-// });const generationConfig = {
-//   temperature: 1,
-//   topP: 0.95,
-//   topK: 64,
-//   maxOutputTokens: 8192,
-//   responseMimeType: 'application/json',
-// };
+
 
